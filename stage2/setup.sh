67d631d30a839632f46a03a8affd89f15b87ca03
Lane Kashur <lkashur@icarusgpvm05.fnal.gov>
2024-07-27 21:39:52 -0500

    Add stage 2 script

--- stage2/setup.sh
+++ stage2/setup.sh
@@ -1,7 +1,9 @@
-#!/bin/bash                
+#!/bin/bash
 source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
 voms-proxy-destroy
-kx509
-voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/icarus/Role=Analysis -valid 168:00            
+kx509 
+voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/icarus/Role=Analysis -valid 168:00
 setup icaruscode v09_82_01 -q e26:prof
 make
+
+
